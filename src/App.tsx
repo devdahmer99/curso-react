a938993c9ac3e73ae15ce602f03951fe20e89db9
devdahmer99 <eduardodahmer99@gmail.com>
2022-08-08 22:42:18 -0300

    Criação de um novo componente e criação da pasta components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
-function App() {
-  return <div>Olá, Mundo!</div>;
+import React from 'react';
+import {Header} from './components/Header';
+
+/*const App = () => {
+  let name: string = 'Eduardo';
+  let n1: number = 2;
+  let n2: number = 4;
+
+function somar(n1: number, n2: number): number {
+  return n1 + n2;
 }
+
+  return (
+    <div>Soma: {somar(n1,n2)}</div>
+  );
+}*/
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      Olá, Mundo!
+      </div>
+  )
+}
+
+
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+    return (
+        <header>
+        <h1>Titulo da Página</h1>
+        <hr />
+        </header>
+    );
+  }
