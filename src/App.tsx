0423e8b01539752e2a844f19c1fdbd6fcefbc480
devdahmer99 <eduardodahmer99@gmail.com>
2022-08-14 21:37:42 -0300

    Aprendendo sobre a criação de eventos no react

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,23 @@ import React from 'react';
 import {Header} from './components/Header';
 import {Photo} from './components/Photo';
 
-/*const App = () => {
-  let name: string = 'Eduardo';
-  let n1: number = 2;
-  let n2: number = 4;
 
-function somar(n1: number, n2: number): number {
-  return n1 + n2;
+const HandleButtonClick = () => {
+    alert('O Botão foi clicado!');
 }
-
-  return (
-    <div>Soma: {somar(n1,n2)}</div>
-  );
-}*/
-
-
 
 const App = () => {
   return (
     <div>
-      <Header title="Este é um exemplo de prop"/>
+      <Header title="Este é um exemplo de uma prop"/>
       
-      Olá, Mundo!
+      <button onClick={HandleButtonClick}>Clique Aqui!</button>
 
       <Photo src="https://www.google.com.br/google.jpg" legend="Google"/>
+
       </div>
   )
 }
-
 
 
 export default App;
