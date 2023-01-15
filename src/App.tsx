4e541319044e63d0f9db8456877ba2f2d839c7ba
Eduardo Dahmer <eduardodahmer99@gmail.com>
2023-01-15 15:09:45 -0300

    Utilizando estilos inline

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 const App = () => {
-    return
-    {
-        <div>
-            
-        </div>
+    const [color, setColor] = useState('#FF0000');
+    const [padding, setPadding] = useState(0);
+
+    const handleClick = () => {
+        setColor('#00FF');
+        setPadding(20);
     }
+
+
+    return (
+     <div>
+        <button
+        onClick={handleClick} 
+        style={{
+            backgroundColor: color,
+            color: '#FFF',
+            border: '0',
+            padding: padding
+        }}>Clique aqui</button>
+     </div>
+    );
 }
 
 export default App;
