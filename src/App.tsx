70798b7e65d1393ca3ad4dfcbd388f486a7d9d60
Eduardo Dahmer <eduardodahmer99@gmail.com>
2023-01-15 20:18:59 -0300

    Trabalhando com a responsividade com tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 const App = () => {
     return (
-        <div>
-            ...
-        </div>
+            <div className="bg-blue-900 text-white p-4 text-2x1 font-bold flex flex-col text-center md:flex-row md:text-green-700">
+                Qualquer texto
+                <button className="bg-green-300 text-black p-2 rounded-xl mt-3 md:ml-3 md:mt-0">Clique Aqui!</button>     
+            </div>
+            
     );
 }
 
